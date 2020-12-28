0c996c136c7ca73a7acc30471058aa55949f318b
Rasmus <50022415+TehRamsus@users.noreply.github.com>
2020-12-28 11:59:49 -0800

    fixed RM_Framework issue :)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ version '1.0.0'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
-	'@RM_Framework/locale.lua',
+	'@es_extended/locale.lua',
 	'locales/fi.lua',
 	'locales/en.lua',
 	'locales/swe.lua',
@@ -15,10 +15,10 @@ server_scripts {
 }
 
 client_scripts {
-	'@RM_Framework/locale.lua',
+	'@es_extended/locale.lua',
 	'locales/fi.lua',
 	'locales/en.lua',
-	'config.lua',
 	'locales/swe.lua',
+	'config.lua',
 	'client/main.lua'
 }
